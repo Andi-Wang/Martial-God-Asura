4080ef35a944c4595af056522f744b1f30ab07dd
Xiao Ye Chen <xchen389@uwo.ca>
2016-12-04 23:34:12 -0500

    Add more ladder triggers and animations

---
+++ game/Assets/Scripts/ActivateLadderAuto.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateLadderAuto : MonoBehaviour {
+    public GameObject ladder;
+    public int ladderId;
+
+    bool alreadyEntered;
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GameObject.FindObjectOfType<Camera>().GetComponent<Animator>();
+        alreadyEntered = false;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!alreadyEntered && other.tag == "Player" && !ladder.activeInHierarchy)
+        {
+            alreadyEntered = true;
+            
+        }
+    }
+}
---
+++ game/Assets/Scripts/LadderTriggerManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LadderTriggerManager : MonoBehaviour {
+
+    public GameObject[] ladders;
+
+	// Use this for initialization
+	void Awake () {
+	    foreach(GameObject ladder in ladders)
+        {
+            if (ladder.activeInHierarchy)
+            {
+                ladder.SetActive(false);
+            }
+        }
+	}
+
+    public void unlockLadder(int ladderId)
+    {
+        ladders[ladderId].SetActive(true);
+    }
+}
