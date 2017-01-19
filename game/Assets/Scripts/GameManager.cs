7f45cc58d66527a57666cd18cb5f7a9110a1114e
Xiao Ye Chen <xchen389@uwo.ca>
2017-01-18 23:42:42 -0500

    Add room mamager

--- game/Assets/Scripts/GameManager.cs
+++ game/Assets/Scripts/GameManager.cs
@@ -6,48 +6,67 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-	public float levelStartDelay = 2f;						//Time to wait before starting level, in seconds.
-	public float turnDelay = 0.1f;							//Delay between each Player turn.
-	public int playerPoints = 100;						//Starting value for Player points.
-	public static GameManager instance = null;				//Static instance of GameManager which allows it to be accessed by any other script.
-	[HideInInspector] public bool playersTurn = true;       //Boolean to check if it's players turn, hidden in inspector but public.
-    public int currentRoom;	
-		
-	private Text levelText;									//Text to display current level number.
-	private GameObject levelImage;							//Image to block out level as levels are being set up, background for levelText.
-	//private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
-	int level = -1;								
-	private List<Enemy> enemies;							//List of all Enemy units, used to issue them move commands.
-	private bool doingSetup = true;							//Boolean to check if we're setting up board, prevent Player from moving during setup.
-		
-	//Awake is always called before any Start functions
-	void Awake()
-	{
-		//Check if instance already exists
-		if (instance == null)
-			instance = this;
-		else if (instance != this)
-            Destroy(gameObject);	
-			
-		//Sets this to not be destroyed when reloading scene
-		DontDestroyOnLoad(gameObject);
-			
-		//Assign enemies to a new List of Enemy objects.
-		enemies = new List<Enemy>();
+    public float levelStartDelay = 2f;                      //Time to wait before starting level, in seconds.
+    public float turnDelay = 0.1f;                          //Delay between each Player turn.
+    public int playerPoints = 100;                      //Starting value for Player points.
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+    [HideInInspector] public bool playersTurn = true;       //Boolean to check if it's players turn, hidden in inspector but public.
+    
+    public int currentRoom;
 
-		//Call the InitGame function to initialize the first level 
-		InitGame();
-	}
-		
+    private Text levelText;                                 //Text to display current level number.
+    private GameObject levelImage;                          //Image to block out level as levels are being set up, background for levelText.
+                                                            //private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
+    int level = -1;
+    private List<Enemy> enemies;                            //List of all Enemy units, used to issue them move commands.
+    private bool doingSetup = true;                         //Boolean to check if we're setting up board, prevent Player from moving during setup.
+    Transform player;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Assign enemies to a new List of Enemy objects.
+        enemies = new List<Enemy>();
+        
+
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+    }
+
+    void Update()
+    {
+        // update the room that player is in
+        // Debug: remove = for release
+        if(level >= 0 && !RoomManager.Instance.inRoom(currentRoom, player.position.x, player.position.y))
+        {
+            currentRoom = RoomManager.Instance.findRoomId(player.position.x, player.position.y);
+        }
+    }
+
 	//Initializes the game for each level.
 	void InitGame()
 	{
 		//While doingSetup is true the player can't move, prevent player from moving while title card is up.
 		doingSetup = true;
         playersTurn = false;
-		
-        if (level > 0)
+
+        // assign player position
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        if (level >= 0) //Debug: remove = for release version
         {
+            // Set up room manager
+            currentRoom = RoomManager.Instance.findRoomId(player.position.x, player.position.y); 
+
             levelImage = GameObject.Find("LevelImage");
 
             //Get a reference to our text LevelText's text component by finding it by name and calling GetComponent.
@@ -129,6 +148,14 @@ public class GameManager : MonoBehaviour
         //Tell our ‘OnLevelFinishedLoading’ function to stop listening for a scene change event as soon as this script is disabled.
         //Remember to always have an unsubscription for every delegate you subscribe to!
         SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    public int Level
+    {
+        get
+        {
+            return level;
+        }
     }
 }
 
---
+++ game/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoomManager : ScriptableObject {
+
+    private Rooms rooms;
+    private static RoomManager instance = null;
+    private string dataPath;
+    
+
+    public static RoomManager Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = CreateInstance<RoomManager>();
+                instance.initRooms();
+            }
+            return instance;
+        }
+    }
+
+    void initRooms()
+    {
+        rooms = new Rooms();
+        int level = GameManager.instance.Level;
+
+        rooms.addRoom(0, level, -8, 0, 27, 13);
+        rooms.addRoom(1, level, 28, 0, 47, 11);
+        rooms.addRoom(2, level, 48, 0, 73, 11);
+        rooms.addRoom(3, level, -7, 14, 20, 26);
+        rooms.addRoom(4, level, 21, 14, 51, 26);
+        rooms.addRoom(5, level, 52, 14, 73, 26);
+        rooms.addRoom(6, level, -7, 29, 10, 40);
+        rooms.addRoom(7, level, 11, 29, 51, 40);
+        rooms.addRoom(8, level, 52, 29, 73, 40);
+        rooms.addRoom(9, level, -7, 43, 73, 60);
+    }
+
+    public int findRoomId(float x, float y)
+    {
+        return rooms.findRoomId(x, y);
+    }
+
+    public bool inRoom(int roomId, float x, float y)
+    {
+        return rooms.inRoom(roomId, x, y);
+    }
+}
+
+public class Room
+{
+    public int level;
+    public float xMin, xMax, yMin, yMax;
+
+    public Room(int lv, float x1, float x2, float y1, float y2)
+    {
+        level = lv;
+        xMin = x1;
+        xMax = x2;
+        yMin = y1;
+        yMax = y2;
+    }
+}
+
+public class Rooms
+{
+    private Dictionary<int, Room> roomDictionary;
+
+    public Rooms()
+    {
+        roomDictionary = new Dictionary<int, Room>();
+    }
+
+    public void addRoom(int roomId, int lv, float x1, float y1, float x2, float y2)
+    {
+        roomDictionary.Add(roomId, new Room(lv, x1, x2, y1, y2));
+    }
+
+    public int findRoomId(float x, float y)
+    {
+        for(int i = 0; i < roomDictionary.Count;++i)
+        {
+            if(inRoom(i, x, y))
+            {
+                return i;
+            }
+        }
+        return 0; // can't find any matching room
+    }
+
+    public bool inRoom(int roomId, float x, float y)
+    {
+        Room targetRoom = roomDictionary[roomId];
+        if (targetRoom.yMax>=y && y >= targetRoom.yMin && targetRoom.xMax >= x && x >= targetRoom.xMin)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
