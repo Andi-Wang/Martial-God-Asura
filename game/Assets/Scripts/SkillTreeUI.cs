3a07122e9536f29b9d685701a9c59d6f40f327f2
Angus <apoole29@uwo.ca>
2017-02-06 21:59:49 -0500

    Updated the skill menu's appearance and layout significantly

--- game/Assets/Scripts/SkillTreeUI.cs
+++ game/Assets/Scripts/SkillTreeUI.cs
@@ -17,6 +17,13 @@ public class SkillTreeUI : MonoBehaviour {
     public GameObject spiritualTree;
 
     public Image background;
+	public Texture bgtxt;
+	public GameObject bgpanel;
+	public Sprite bg1;
+	public Sprite bg2;
+	public Sprite bg3;
+
+	public Text pgtitle;
 
     public Button[] buttons = new Button[NUM_SKILL_TREE_BRANCHES * NUM_SKILL_TREE_TIERS * SKILLS_PER_TIER];
 
@@ -29,6 +36,10 @@ public class SkillTreeUI : MonoBehaviour {
     const int ENHANCEMENT_BRANCH = 6;
     const int FORMATION_BRANCH = 7;
     const int SUMMONING_BRANCH = 8;
+
+	RectTransform maRT;
+	RectTransform cuRT;
+	RectTransform spRT;
 
     SkillTree tree;
 
@@ -59,7 +70,9 @@ public class SkillTreeUI : MonoBehaviour {
             buttons[temp].onClick.AddListener(() => clickListener(temp));
         }
 
-
+		maRT = martialArtsToggle.GetComponent<RectTransform>();
+		cuRT = cultivationToggle.GetComponent<RectTransform>();
+		spRT = spiritualToggle.GetComponent<RectTransform>();
         //If we want to set color in the script
         /*
         martialArtsToggle.GetComponentInChildren<Image>().color = Color.red;
@@ -74,25 +87,97 @@ public class SkillTreeUI : MonoBehaviour {
             martialArtsTree.SetActive(true);
             cultivationTree.SetActive(false);
             spiritualTree.SetActive(false);
-            background.color = martialArtsToggle.GetComponentInChildren<Image>().color;
+			bgpanel.GetComponent<Image> ().sprite = bg1;
+			if ((Vector2)maRT.localPosition != new Vector2 (maRT.localPosition.x, -40)) {
+				maRT.localPosition = new Vector2 (maRT.localPosition.x, -40);
+			}
+			if ((Vector2)cuRT.localPosition != new Vector2 (cuRT.localPosition.x, -20)) {
+				cuRT.localPosition = new Vector2 (cuRT.localPosition.x, -20);
+			}
+			if ((Vector2)spRT.localPosition != new Vector2 (spRT.localPosition.x, -22)) {
+				spRT.localPosition = new Vector2 (spRT.localPosition.x, -22);
+			}
+			maRT.sizeDelta = new Vector2 (108, 125);
+			maRT.GetChild (0).GetComponent<RectTransform> ().sizeDelta = new Vector2(104, 120);
+			cuRT.sizeDelta = new Vector2 (108, 62);
+			cuRT.GetChild (0).GetComponent<RectTransform> ().sizeDelta = new Vector2(104, 62);
+			spRT.sizeDelta = new Vector2 (108, 62);
+			spRT.GetChild (0).GetComponent<RectTransform> ().sizeDelta = new Vector2(104, 62);
+			pgtitle.text = "Martial Arts";
+            //background.color = martialArtsToggle.GetComponentInChildren<Image>().color;
         }
         else if(cultivationToggle.isOn) {
             martialArtsTree.SetActive(false);
             cultivationTree.SetActive(true);
             spiritualTree.SetActive(false);
-            background.color = cultivationToggle.GetComponentInChildren<Image>().color;
+			bgpanel.GetComponent<Image> ().sprite = bg2;
+			if ((Vector2)maRT.localPosition != new Vector2 (maRT.localPosition.x, -20)) {
+				maRT.localPosition = new Vector2 (maRT.localPosition.x, -20);
+			}
+			if ((Vector2)cuRT.localPosition != new Vector2 (cuRT.localPosition.x, -40)) {
+				cuRT.localPosition = new Vector2 (cuRT.localPosition.x, -40);
+			}
+			if ((Vector2)spRT.localPosition != new Vector2 (spRT.localPosition.x, -22)) {
+				spRT.localPosition = new Vector2 (spRT.localPosition.x, -22);
+			}
+			maRT.sizeDelta = new Vector2 (108, 62);
+			maRT.GetChild (0).GetComponent<RectTransform> ().sizeDelta = new Vector2(104, 62);
+			cuRT.sizeDelta = new Vector2 (108, 125);
+			cuRT.GetChild (0).GetComponent<RectTransform> ().sizeDelta = new Vector2(104, 120);
+			spRT.sizeDelta = new Vector2 (108, 62);
+			spRT.GetChild (0).GetComponent<RectTransform> ().sizeDelta = new Vector2(104, 62);
+			pgtitle.text = "Cultivation";
+			//maToggle.transform;
+            //background.color = cultivationToggle.GetComponentInChildren<Image>().color;
         }
         else if(spiritualToggle.isOn) {
             martialArtsTree.SetActive(false);
             cultivationTree.SetActive(false);
             spiritualTree.SetActive(true);
-            background.color = spiritualToggle.GetComponentInChildren<Image>().color;
+			bgpanel.GetComponent<Image> ().sprite = bg3;
+			if ((Vector2)maRT.localPosition != new Vector2 (maRT.localPosition.x, -20)) {
+				maRT.localPosition = new Vector2 (maRT.localPosition.x, -20);
+			}
+			if ((Vector2)cuRT.localPosition != new Vector2 (cuRT.localPosition.x, -20)) {
+				cuRT.localPosition = new Vector2 (cuRT.localPosition.x, -20);
+			}
+			if ((Vector2)spRT.localPosition != new Vector2 (spRT.localPosition.x, -40)) {
+				spRT.localPosition = new Vector2 (spRT.localPosition.x, -40);
+			}
+			maRT.sizeDelta = new Vector2 (108, 62);
+			maRT.GetChild (0).GetComponent<RectTransform> ().sizeDelta = new Vector2(104, 62);
+			cuRT.sizeDelta = new Vector2 (108, 62);
+			cuRT.GetChild (0).GetComponent<RectTransform> ().sizeDelta = new Vector2(104, 62);
+			spRT.sizeDelta = new Vector2 (108, 125);
+			spRT.GetChild (0).GetComponent<RectTransform> ().sizeDelta = new Vector2(104, 120);
+			pgtitle.text = "\t\tSpirit";
+            //background.color = spiritualToggle.GetComponentInChildren<Image>().color;
         }
         else {
             martialArtsTree.SetActive(false);
             cultivationTree.SetActive(false);
             spiritualTree.SetActive(false);
-            background.color = Color.white;
+            //background.color = Color.white;
         }
 	}
+
+	private float originalWidth = 640.0f;  // define here the original resolution
+	private float originalHeight = 400.0f; // you used to create the GUI contents 
+	private Vector3 scale;
+
+	/*void OnGUI(){
+		scale.x = Screen.width/originalWidth; // calculate hor scale
+		scale.y = Screen.height/originalHeight; // calculate vert scale
+		scale.z = 1;
+		var svMat = GUI.matrix; // save current matrix
+		// substitute matrix - only scale is altered from standard
+		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, scale);
+		// draw your GUI controls here:
+		//GUI.Box(new Rect(10,10,600,400), bgtxt);
+		GUI.DrawTexture(new Rect(10, 10, 600, 400), bgtxt, ScaleMode.ScaleToFit, true, 0.0F);
+		//GUI.Button(new Rect(300,100,50,50), "Button");
+		//...
+		// restore matrix before returning
+		GUI.matrix = svMat; // restore matrix
+	}*/
 }
