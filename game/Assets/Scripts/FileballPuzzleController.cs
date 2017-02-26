ae896a0bf7eb80d99229a54e357200cae1871690
Xiao Ye Chen <xchen389@uwo.ca>
2017-02-26 17:18:49 -0500

    Add fireball puzzle

---
+++ game/Assets/Scripts/FileballPuzzleController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FileballPuzzleController : MonoBehaviour {
+    public float m_maxTravelTime = 0.2f;
+    public float m_maxDamage = 5f;
+    public float m_speed = 1f;
+
+    float timeCounter = 0;
+    GameObject player;
+    UnityStandardAssets._2D.PlatformerCharacter2D playerScript;
+
+    void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>();
+    }
+
+    void Update()
+    {
+        transform.position += transform.right * m_speed * Time.deltaTime;
+        timeCounter += Time.deltaTime;
+        if (timeCounter>= m_maxTravelTime)
+        {
+            ChangeDirection();
+            timeCounter = 0;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            playerScript.TakeDamage(m_maxDamage);
+        }   
+    }
+    
+    void ChangeDirection()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        transform.right *= -1;
+    }
+}
